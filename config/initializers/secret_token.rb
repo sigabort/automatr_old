09da0eda68751945f3a50b9b5ce222c15ffc51ce
superguy <superguy@super.com>
2010-06-22 20:04:29 -0400

    Initial commit for automatr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '899b8110391148a6d800b9ed9a20a2095f8bf7c81b95572f0c03653dd0ade050f1902183b70c5ceb62192d892dfce1414f3aaecb795a99532d25f0ecae9ff5fc'
