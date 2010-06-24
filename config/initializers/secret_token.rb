a96f15582474073903a0f8fd355db2836a603907
sigabort <abort.coredump@gmail.com>
2010-06-24 01:02:52 -0400

    first commit for automatr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'a9b1f738ed73f8a248bbfba1d414f041c0fe8d5ee2ec013868d92ee5e763625f86207927d306de5f2a4a0568be88054f3710f3b273ef5786176db052cae26984'
